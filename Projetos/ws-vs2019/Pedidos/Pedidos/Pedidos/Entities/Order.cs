dd196126cbb30614b87416360bc9009c3692302f
Romulo Conti <romulo.conti@gmail.com>
2021-04-24 00:36:06 -0300

    Sudindo Projeto de Pedidos

---
+++ Projetos/ws-vs2019/Pedidos/Pedidos/Pedidos/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pedidos.Entities.Enums;
+
+namespace Pedidos.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+
+    }
+}
