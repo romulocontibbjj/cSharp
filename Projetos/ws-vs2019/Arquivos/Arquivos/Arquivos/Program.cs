94e82ecdb3105d5c3eb670389497dab23de2b719
Romulo Conti <romulo.conti@gmail.com>
2021-04-26 00:20:02 -0300

    Trabalhando com Arquivos

---
+++ Projetos/ws-vs2019/Arquivos/Arquivos/Arquivos/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio1();
+            //Exercicio2();
+            //Exercicio3();
+            //Exercicio4();
+            //Exercicio5();
+            //Exercicio6();
+            //Exercicio7();
+            //Exercicio8();
+            ExercicioDeFixacao();
+        }
+
+        static void Exercicio1()
+        {
+            string sourcePath = @"C:\tmp\file1.txt";
+            string targetPath = @"C:\tmp\file2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                //fileinfo.CopyTo(targetPath.ToString());
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line.ToString());
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+
+            }
+
+
+        }
+
+        static void Exercicio2()
+        {
+            string path = @"C:\tmp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+
+        }
+
+        static void Exercicio3()
+        {
+            string path = @"C:\tmp\file1.txt";
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+
+            }
+        }
+
+        static void Exercicio4()
+        {
+            string path = @"C:\tmp\file1.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Aconteceu um erro");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void Exercicio5()
+        {
+            string path = @"C:\tmp\file1.txt";
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Aconteceu um erro");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        
+        static void Exercicio6() //Escrita de Daodos
+        {
+            string sourcepath = @"C:\tmp\file1.txt";
+            string targetpath = @"C:\tmp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e )
+            {
+                Console.WriteLine("Ocorreu um erro");
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+
+        static void Exercicio7()//Operações com pastas
+        {
+            string path = @"c:\tmp\MyFolder";
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*",SearchOption.AllDirectories);
+                //or var folders = Directory.EnumerateDirectories(path, "*.*",SearchOption.AllDirectories);
+                Console.WriteLine("Folders:");
+                
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("");
+                //ver arquivos
+                Console.WriteLine("Files:");
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach(string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                //Criar Pasta
+                if (!Directory.Exists(path + "\\newFolder2"))
+                {
+                    Directory.CreateDirectory(path + "\\newFolder2");
+                }
+                
+
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Exercicio8()//Path
+        {
+            string path = @"C:\Tmp\MyFolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: "+ Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: "+ Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: "+ Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: "+ Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: "+ Path.GetTempPath());
+        }
+
+        static void ExercicioDeFixacao()
+        {
+            string path = @"C:\Tmp\MyFolder\file.csv";
+            string outPath =@"C:\Tmp\MyFolder\out\file.csv";
+
+            if (!Directory.Exists(Path.GetDirectoryName(outPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+            }
+
+            List<string> listaProdutos = new List<string>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(",");
+                    string nome = line[0];
+                    double valor = Double.Parse(line[1], CultureInfo.InvariantCulture);
+                    int quantidade = int.Parse(line[2]);
+
+                    Console.WriteLine("Produto: " + nome + ", Valor: " + valor.ToString("F2", CultureInfo.InvariantCulture) +
+                                    ", Quantidade: " + quantidade);
+
+                    listaProdutos.Add(nome + "," + (valor * quantidade).ToString("F2", CultureInfo.InvariantCulture) );
+
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Lista de Produtos");
+            foreach (string s in listaProdutos)
+            {
+                Console.WriteLine(s);
+            }
+
+
+
+        }
+    }
+}
--- Projetos/ws-vs2019/Heranca/Heranca/Heranca/Program.cs
+++ Projetos/ws-vs2019/Heranca/Heranca/Heranca/Program.cs
@@ -7,12 +7,61 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
-            Console.WriteLine(account.Balance);
-
-            account.Deposit(200.0);
-
-            Console.WriteLine(account.Balance);
+            //Ex1();
+            Ex2();
         }
+
+        static void Ex1()
+        {
+            //BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            //Console.WriteLine(account.Balance);
+            //account.Deposit(200.0);
+            //Console.WriteLine(account.Balance);
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //Upcasting -> da SubClasse para a Super-Classe
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            //DownCasting -> da Super-Classe para a SubClasse
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3; //Erro - Tipos Incompatívels
+
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3; ou cod abaixo
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3; desde jeito ou como no código abaixo "as"
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+        }
+
+        static void Ex2()
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance.ToString());
+            Console.WriteLine(acc2.Balance.ToString());
+        }
+
     }
+
 }
